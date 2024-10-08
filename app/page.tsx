68b1210a3b649576d8e86787ab66e354f87c190e
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-10-08 16:27:06 +0800

    feat: update config

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
-"use client";
+'use client';
+
+import { useEffect } from 'react';
 
 export default function Home() {
+  useEffect(() => {
+    console.log('non-strictMode render only once');
+  }, []);
   return (
     <>
       <div>长风几万里 吹度玉门关</div>
--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,75 @@
 /** @type {import('next').NextConfig} */
+const mode = process.env.BUILD_MODE ?? 'standalone';
 const nextConfig = {
-  output: 'export',
+  output: mode,
   distDir: 'dist',
   reactStrictMode: false
 };
+const CorsHeaders = [
+  { key: 'Access-Control-Allow-Credentials', value: 'true' },
+  { key: 'Access-Control-Allow-Origin', value: '*' },
+  {
+    key: 'Access-Control-Allow-Methods',
+    value: '*'
+  },
+  {
+    key: 'Access-Control-Allow-Headers',
+    value: '*'
+  },
+  {
+    key: 'Access-Control-Max-Age',
+    value: '86400'
+  }
+];
+
+if (mode !== 'export') {
+  nextConfig.headers = async () => {
+    return [
+      {
+        source: '/api/:path*',
+        headers: CorsHeaders
+      }
+    ];
+  };
+
+  nextConfig.rewrites = async () => {
+    const ret = [
+      // adjust for previous version directly using "/api/proxy/" as proxy base route
+      // {
+      //   source: "/api/proxy/v1/:path*",
+      //   destination: "https://api.openai.com/v1/:path*",
+      // },
+      {
+        // https://{resource_name}.openai.azure.com/openai/deployments/{deploy_name}/chat/completions
+        source: '/api/proxy/azure/:resource_name/deployments/:deploy_name/:path*',
+        destination: 'https://:resource_name.openai.azure.com/openai/deployments/:deploy_name/:path*'
+      },
+      {
+        source: '/api/proxy/google/:path*',
+        destination: 'https://generativelanguage.googleapis.com/:path*'
+      },
+      {
+        source: '/api/proxy/openai/:path*',
+        destination: 'https://api.openai.com/:path*'
+      },
+      {
+        source: '/api/proxy/anthropic/:path*',
+        destination: 'https://api.anthropic.com/:path*'
+      },
+      {
+        source: '/google-fonts/:path*',
+        destination: 'https://fonts.googleapis.com/:path*'
+      },
+      {
+        source: '/sharegpt',
+        destination: 'https://sharegpt.com/api/conversations'
+      }
+    ];
+
+    return {
+      beforeFiles: ret
+    };
+  };
+}
 
 export default nextConfig;
