7e0c82b12770f965024afdc887a6e739b3cbe68d
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-10-08 13:21:58 +0800

    feat: update launch.json

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  distDir: "dist",
+  output: 'export',
+  distDir: 'dist',
+  reactStrictMode: false
 };
 
 export default nextConfig;
