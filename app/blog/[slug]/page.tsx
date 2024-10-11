ddcf547599c882b9f25ff8f5d261c4ce3206b7c2
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-10-11 14:27:17 +0800

    feat: update config data

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -1,7 +1,25 @@
-// 'use client';
 import { FC } from 'react';
 
-const BlogSlug: FC<any> = ({ params }) => {
+const fetchPosts = async () => {
+  return [{ slug: 'english' }];
+};
+
+export async function generateStaticParams() {
+  // 从数据源获取所有博客文章的 slug
+  const posts = await fetchPosts(); // 你需要根据实际情况修改 fetchPosts 函数
+
+  return posts.map((post) => ({
+    slug: post.slug
+  }));
+}
+
+interface BlogSlugProps {
+  params: {
+    slug: string;
+  };
+}
+
+const BlogSlug: FC<BlogSlugProps> = ({ params }) => {
   return (
     <div>
       <h1>blog {params.slug}</h1>
