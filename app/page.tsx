81b2950e86d9eb17006d9aba3b7d844ebfc35f29
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-10-15 14:52:14 +0800

    chore: update env

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ import { useEffect } from 'react';
 
 export default function Home() {
   useEffect(() => {
+    console.log(process.env.NEXT_PUBLIC_YYCF);
     console.log('non-strictMode render only once');
   }, []);
   return <></>;
