3d83f1af77245dc3a3c35789f4270aab39753cfb
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-11-19 10:49:01 +0800

    feat: add React use for page async getData

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,17 @@
-// 'use client';
 import { FC } from 'react';
+import { sleep } from '../utils';
 
-const DashBoard: FC = () => {
+async function getData() {
+  await sleep(5000);
+  return {
+    message: 'Hello, DashBoard!'
+  };
+}
+const DashBoard: FC = async () => {
+  const { message } = await getData();
   return (
-    <div>
-      <h1>Dashboard</h1>
+    <div className="h-60 mt-5 flex-1 rounded-xl bg-indigo-500 text-white flex items-center justify-center">
+      {message}
     </div>
   );
 };
