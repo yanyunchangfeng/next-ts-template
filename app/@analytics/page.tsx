8ab7eb6293d5d47162d444af86db3e2442f9f65e
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-11-22 19:17:23 +0800

    feat: add parallel route loading

--- app/@analytics/page.tsx
+++ app/@analytics/page.tsx
@@ -1,6 +1,8 @@
 import { FC } from 'react';
+import { sleep } from '../utils';
 
-const Analytics: FC = () => {
+const Analytics: FC = async () => {
+  await sleep(1000 * 5);
   return <h1>Analytics</h1>;
 };
 export default Analytics;
---
+++ app/@team/loading.tsx
@@ -0,0 +1,8 @@
+const Loading = () => {
+  return (
+    <div className="flex justify-center items-center bg-red-500">
+      <div className="animate-spin rounded-full h-32 w-32 border-b-2 border-gray-900">loading team</div>
+    </div>
+  );
+};
+export default Loading;
