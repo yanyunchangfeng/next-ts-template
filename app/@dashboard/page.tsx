2f66e38ae7f17a8dd32e98ce7aedf3e8c67da863
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-11-24 20:17:34 +0800

    style: update style

--- app/@dashboard/page.tsx
+++ app/@dashboard/page.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react';
 
 const DashBoard: FC = () => {
   return (
-    <div className="h-60 flex-1 rounded-xl bg-indigo-500 text-white flex items-center justify-center">
+    <div className="h-60 flex-1 rounded-xl bg-indigo-500 text-white flex items-center justify-center mb-6">
       Hello, DashBoard!
     </div>
   );
