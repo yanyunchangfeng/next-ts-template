479fba57e7dce677c00367cfdb48415df378d24f
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-11-27 14:24:24 +0800

    refactor: api route

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,15 +1,20 @@
 // app/api/search/route.ts
 // 2.1. 如何获取网址参数？
-import {
-  // NextRequest,
-  NextResponse
-} from 'next/server';
+import { isVercel } from '@/app/shared';
+import { NextRequest, NextResponse } from 'next/server';
 
 // 访问 /api/search?query=react
-export function GET() {
-  // request: NextRequest
+const dynamicGet = async (request: NextRequest) => {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get('query'); // query
+  console.log(query);
+  return NextResponse.json({ query }, { status: 200 });
+};
+const staticGet = async () => {
   // const searchParams = request.nextUrl.searchParams;
   // const query = searchParams.get('query'); // query
   // console.log(query);
-  return NextResponse.json({ query: 'react' }, { status: 200 });
-}
+  return NextResponse.json({ query: 'nextjs' }, { status: 200 });
+};
+
+export const GET = isVercel ? dynamicGet : staticGet;
