92d7e2cbcb7e3d76c70d0c626a3f2bbf9c9b98ff
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-11-27 11:47:02 +0800

    chore: update shared

--- app/shared/entities/index.ts
+++ app/shared/entities/index.ts
@@ -1,12 +1,1 @@
-export interface ToDo {
-  userId: number;
-  id: number;
-  title: string;
-  completed: boolean;
-}
-
-export interface PhotoParams {
-  params: {
-    id: string;
-  };
-}
+export * from './photo';
