1c13dfe72fa0660b44fc341332e6a5c27e9677d6
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-11-29 13:00:36 +0800

    feat: add middlewares & chain

---
+++ app/middlewares/withRedirect.ts
@@ -0,0 +1,20 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+export const withRedirect = (
+  index: number
+  // next:Function,
+) => {
+  return async (request: NextRequest) => {
+    console.log(`Middleware withRedirect index is ${index} running`, request.url);
+    // 条件语句
+    // 第二种方法是使用条件语句：(两种不能合用 配置了matcher 只有matcher匹配才会生效)
+    // if (request.nextUrl.pathname.startsWith('/about/')) {
+    //   return NextResponse.rewrite(new URL('/', request.url));
+    // }
+    // if (request.nextUrl.pathname.startsWith('/dashboard/')) {
+    //   return NextResponse.rewrite(new URL('/', request.url)); // 重写不会变更浏览器地址栏
+    // }
+    return NextResponse.redirect(new URL('/', request.url)); // matcher config 对应的处理逻辑
+    // return next(request);
+  };
+};
