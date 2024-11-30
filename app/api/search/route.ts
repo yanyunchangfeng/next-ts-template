fbd527554b502758deb74a13724df3260f1b433c
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-11-30 10:25:40 +0800

    chore: refactor: api route

--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,9 +1,9 @@
 // app/api/search/route.ts
 // 2.1. 如何获取网址参数？
-import { isStatic, isVercel } from '@/app/shared';
+import { isDynamic } from '@/app/shared';
 import { NextRequest, NextResponse } from 'next/server';
 
-export const dynamic = isVercel || !isStatic ? 'force-dynamic' : 'force-static';
+export const dynamic = isDynamic ? 'force-dynamic' : 'force-static';
 
 // 访问 /api/search?query=react
 export const GET = async (request: NextRequest) => {
