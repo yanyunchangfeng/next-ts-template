2c6022162352edd44dec077bffedbcb89effc58c
yanyunchangfeng <yanyunchangfeng@gmail.com>
2024-12-02 15:01:09 +0800

    style: update focus ring

--- app/notes/components/AddNote/index.tsx
+++ app/notes/components/AddNote/index.tsx
@@ -9,7 +9,7 @@ export const AddNote: FC = () => {
       <Textarea
         rows={2}
         placeholder="Add a new note"
-        className="border bg-transparent p-2 rounded-md shadow-md flex-1 focus:border-pink-500 focus:outline-none"
+        className="focus:ring bg-transparent p-2 rounded-md shadow-md flex-1 focus:ring-pink-500 focus:outline-none"
         value={addNoteTitle}
         onChange={(e) => setAddNoteTitle(e.target.value)}
       />
